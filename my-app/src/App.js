2cc33f2a656122d348b94c91740ddac81a940e58
jihad <jihad.mohammed95@gmail.com>
2023-04-26 15:06:24 +0200

    weather in london

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [weather, setWeather] = useState([])
+  const fetchData = () => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=5b0d4dee87c8fc0237afdfcdd93b4873')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setWeather(data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+ 
+  const temperature = weather.main ? weather.main.temp : null;
+  const description = weather.weather ? weather.weather[0].description : null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    temperature in london is {temperature} fahrenheit and the weather is {description} 
+   </div>
   );
 }
 
