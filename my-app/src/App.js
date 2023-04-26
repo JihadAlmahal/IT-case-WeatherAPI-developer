b77be34fd16dc98750a034a59e711cd7d4263a9b
jihad <jihad.mohammed95@gmail.com>
2023-04-26 17:38:40 +0200

    Enter city name to get weather

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import './App.css';
 
 function App() {
   const [weather, setWeather] = useState([])
+  const [location, setLocation] = useState();
+  const [lastLocation, setLastLocation] = useState();
   const fetchData = () => {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=5b0d4dee87c8fc0237afdfcdd93b4873')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`)
       .then(response => {
         return response.json()
       })
@@ -15,15 +17,33 @@ function App() {
         console.error(error);
       });
   }
-  useEffect(() => {
-    fetchData()
-  }, [])
- 
+
+
   const temperature = weather.main ? weather.main.temp : null;
   const description = weather.weather ? weather.weather[0].description : null;
+
+  const handleLocationChange = event => {
+    setLocation(event.target.value);
+  }
+  const handlClick = () => {
+    setLastLocation(location);
+    fetchData();
+  }
+  
   return (
+    <div>
+    <div>
+        <label htmlFor="location-input">Enter a city:</label>
+        <input type="text" id="location-input" value={location} onChange={handleLocationChange} />
+        <button onClick={handlClick}>Go</button>
+      </div>
    <div>
-    temperature in london is {temperature} fahrenheit and the weather is {description} 
+   {temperature && description ? (
+      `Temperature in ${lastLocation} is ${temperature} kelvin and the weather is ${description}.`
+    ) : (
+      "city not available"
+    )}
+   </div>
    </div>
   );
 }
